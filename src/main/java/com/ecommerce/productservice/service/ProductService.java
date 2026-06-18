f38ebfddb6b8c0bfb61ba1a4944a06cc96661da2
Nimasha <nimasha@ftservices.net>
2026-06-18 21:38:25 +0530

    Create Product API
    Add Create Product API

---
+++ src/main/java/com/ecommerce/productservice/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.ecommerce.productservice.service;
+
+import com.ecommerce.productservice.entity.Product;
+import com.ecommerce.productservice.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private  final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
