568e49fa7636e0e7391b1c8c2a5ffe6c4a7d2392
dew06 <ransikadewmini06@gmail.com>
2026-06-19 17:03:49 +0530

    feat: add Get Product by ID endpoint

--- src/main/java/com/ecommerce/productservice/controller/ProductController.java
+++ src/main/java/com/ecommerce/productservice/controller/ProductController.java
@@ -3,6 +3,9 @@ package com.ecommerce.productservice.controller;
 import com.ecommerce.productservice.entity.Product;
 import com.ecommerce.productservice.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
@@ -33,4 +36,22 @@ public class ProductController {
     public ResponseEntity<Product> createProduct(@RequestBody Product product){
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(product));
     }
+
+    @Operation(
+            summary = "Get a product by ID",
+            description = "Retrieves a single product using its unique ID"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Product found",
+                    content = @Content(schema = @Schema(implementation = Product.class))),
+            @ApiResponse(responseCode = "404", description = "Product not found",
+                    content = @Content(schema = @Schema(example = "Product with id 99 not found")))
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(
+            @Parameter(description = "ID of the product to retrieve", example = "1", required = true)
+            @PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
 }
+
--- src/main/java/com/ecommerce/productservice/service/ProductService.java
+++ src/main/java/com/ecommerce/productservice/service/ProductService.java
@@ -3,6 +3,8 @@ package com.ecommerce.productservice.service;
 import com.ecommerce.productservice.entity.Product;
 import com.ecommerce.productservice.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+import com.ecommerce.productservice.exception.ProductNotFoundException;
+
 
 @Service
 public class ProductService {
@@ -15,5 +17,10 @@ public class ProductService {
 
     public Product createProduct(Product product) {
         return productRepository.save(product);
+    }
+
+     public Product getProductById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(id));
     }
 }
